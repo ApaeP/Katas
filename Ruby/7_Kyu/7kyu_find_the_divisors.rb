c749f19465e1c69d894b5f3f43fb85d3ef078380
Paul Portier <paulportier@gmail.com>
2021-05-08 00:38:16 +0200

    Last resolved : (Find the divisors 7kyu)

---
+++ Ruby/7_Kyu/7kyu_find_the_divisors.rb
@@ -0,0 +1,17 @@
+# Codewars - Kata Solution
+# FIND THE DIVISORS (7 kyu)
+
+# Instructions
+# Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+# Example:
+# divisors(12) # should return [2,3,4,6]
+# divisors(25) # should return [5]
+# divisors(13) # should return "13 is prime"
+
+# Solution
+def divisors(n)
+  (a = (2..n - 1).select { |x| n % x == 0 } ).empty? ? "#{n} is prime" : a
+end
+
+
+# codewars_id: 544aed4c4a30184e960010f4   |   10/01/2021 - 00:32:09
