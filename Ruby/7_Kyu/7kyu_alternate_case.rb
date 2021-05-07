6b87909b4f2e5745f4c85b105e8be5303c37fdd1
Paul Portier <paulportier@gmail.com>
2021-05-08 00:37:19 +0200

    Last resolved : (Alternate case 7kyu)

---
+++ Ruby/7_Kyu/7kyu_alternate_case.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# ALTERNATE CASE (7 kyu)
+
+# Instructions
+# Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+
+# Solution
+def alternate_case(s)
+  s.swapcase
+end
+
+
+# codewars_id: 57a62154cf1fa5b25200031e   |   12/04/2021 - 21:32:00
