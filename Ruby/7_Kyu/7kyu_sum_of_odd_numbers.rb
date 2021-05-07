4805811d301b3ea388a7402c3f67c281c0f5b66e
Paul Portier <paulportier@gmail.com>
2021-05-08 00:39:00 +0200

    Last resolved : (Sum of odd numbers 7kyu)

---
+++ Ruby/7_Kyu/7kyu_sum_of_odd_numbers.rb
@@ -0,0 +1,22 @@
+# Codewars - Kata Solution
+# SUM OF ODD NUMBERS (7 kyu)
+
+# Instructions
+# Given the triangle of consecutive odd numbers:
+#              1
+#           3     5
+#        7     9    11
+#    13    15    17    19
+# 21    23    25    27    29
+# ...
+# Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.:
+# row_sum_odd_numbers(1); # 1
+# row_sum_odd_numbers(2); # 3 + 5 = 8
+
+# Solution
+def row_sum_odd_numbers(n)
+  n**3
+end
+
+
+# codewars_id: 55fd2d567d94ac3bc9000064   |   05/01/2021 - 00:46:47
