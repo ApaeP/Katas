2c13f8a3ec652199415bc054f6bb29a70d9f5673
Paul Portier <paulportier@gmail.com>
2021-05-08 00:37:31 +0200

    Last resolved : (Thinkful  String Drills Repeater 7kyu)

---
+++ Ruby/7_Kyu/7kyu_thinkful_string_drills_repeater.rb
@@ -0,0 +1,17 @@
+# Codewars - Kata Solution
+# THINKFUL  STRING DRILLS REPEATER (7 kyu)
+
+# Instructions
+# Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+# Example:
+# Repeater.repeat("a", 5)
+# should return
+# "aaaaa"
+
+# Solution
+def repeater(string, n)
+  string * n
+end
+
+
+# codewars_id: 585a1a227cb58d8d740001c3   |   31/03/2021 - 22:59:40
