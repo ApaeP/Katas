0ee2af8cedf1ab8b942370d0f54c2cfc485eddc2
Paul Portier <paulportier@gmail.com>
2021-05-08 00:37:20 +0200

    Last resolved : (Digitize 7kyu)

---
+++ Ruby/7_Kyu/7kyu_digitize.rb
@@ -0,0 +1,19 @@
+# Codewars - Kata Solution
+# DIGITIZE (7 kyu)
+
+# Instructions
+# Given a non-negative integer, return an array / a list of the individual digits in order.
+# Examples:
+# 123 => [1,2,3]
+
+# 1 => [1]
+
+# 8675309 => [8,6,7,5,3,0,9]
+
+# Solution
+def digitize(n)
+  n.digits.reverse
+end
+
+
+# codewars_id: 5417423f9e2e6c2f040002ae   |   12/04/2021 - 21:28:42
