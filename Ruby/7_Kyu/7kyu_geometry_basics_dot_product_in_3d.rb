06b9f9d48d00356a76f9888e59e7639c701ede1c
Paul Portier <paulportier@gmail.com>
2021-05-08 00:37:57 +0200

    Last resolved : (Geometry Basics Dot Product in 3D 7kyu)

---
+++ Ruby/7_Kyu/7kyu_geometry_basics_dot_product_in_3d.rb
@@ -0,0 +1,17 @@
+# Codewars - Kata Solution
+# GEOMETRY BASICS DOT PRODUCT IN 3D (7 kyu)
+
+# Instructions
+# This series of katas will introduce you to basics of doing geometry with computers.
+# Vector objects have x, y, and z attributes.
+# Write a function calculating dot product of Vector a and Vector b.
+# You can read more about dot product on Wikipedia.
+# Tests round answers to 6 decimal places.
+
+# Solution
+def dot_product(a, b)
+  a.x * b.x + a.y * b.y + a.z * b.z
+end
+
+
+# codewars_id: 58e3ea29a33b52c1dc0000c0   |   16/01/2021 - 15:43:01
