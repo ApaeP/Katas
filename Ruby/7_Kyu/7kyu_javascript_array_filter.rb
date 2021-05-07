18dd2caff96489a8efd5c7153d38cbedf9cb4461
Paul Portier <paulportier@gmail.com>
2021-05-08 00:38:22 +0200

    Last resolved : (JavaScript Array Filter 7kyu)

---
+++ Ruby/7_Kyu/7kyu_javascript_array_filter.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# JAVASCRIPT ARRAY FILTER (7 kyu)
+
+# Instructions
+# The solution would work like the following:
+# get_even_numbers([2,4,5,6]) => [2,4,6]
+
+# Solution
+def get_even_numbers(arr)
+  arr.select(&:even?)
+end
+
+
+# codewars_id: 514a6336889283a3d2000001   |   08/01/2021 - 15:09:36
