b1e3fafee1803fb8d4d0b1aed39e9eb6ab06cc80
Paul Portier <paulportier@gmail.com>
2021-05-08 14:32:29 +0200

    Last resolved : (Grasshopper  Messi goals function 8kyu)

---
+++ Ruby/8_Kyu/8kyu_grasshopper_messi_goals_function.rb
@@ -0,0 +1,21 @@
+# Codewars - Kata Solution
+# GRASSHOPPER  MESSI GOALS FUNCTION (8 kyu)
+
+# Instructions
+# Messi goals function
+# Messi is a soccer player with goals in three leagues:
+# LaLiga
+# Copa del Rey
+# Champions
+# Complete the function to return his total number of goals in all three leagues.
+# Note: the input will always be valid.
+# For example:
+# 5, 10, 2  -->  17
+
+# Solution
+def goals (x, y, z)
+  x+y+z
+end
+
+
+# codewars_id: 55f73be6e12baaa5900000d4   |   30/10/2020 - 00:51:01
