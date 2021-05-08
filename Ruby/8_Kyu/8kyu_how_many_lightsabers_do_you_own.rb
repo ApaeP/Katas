38a34dfe7f8d3a04c8ea4e9aeb30fe1cb1419481
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:13 +0200

    Last resolved : (How many lightsabers do you own 8kyu)

---
+++ Ruby/8_Kyu/8kyu_how_many_lightsabers_do_you_own.rb
@@ -0,0 +1,20 @@
+# Codewars - Kata Solution
+# HOW MANY LIGHTSABERS DO YOU OWN (8 kyu)
+
+# Instructions
+# Inspired by the development team at Vooza, write the function that
+# accepts the name of a programmer, and
+# returns the number of lightsabers owned by that person.
+# The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+# Note: your function should have a default parameter.
+# For example(Input --> Output):
+# "anyone else" --> 0
+# "Zach" --> 18
+
+# Solution
+def how_many_light_sabers_do_you_own(name = '')
+  name == 'Zach' ? 18 : 0
+end
+
+
+# codewars_id: 51f9d93b4095e0a7200001b8   |   01/11/2020 - 20:40:30
