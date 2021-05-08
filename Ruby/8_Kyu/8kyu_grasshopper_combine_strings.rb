e7d87289ac8ff834d478286a220dc7036641ed58
Paul Portier <paulportier@gmail.com>
2021-05-08 14:23:10 +0200

    Last resolved : (Grasshopper  Combine strings 8kyu)

---
+++ Ruby/8_Kyu/8kyu_grasshopper_combine_strings.rb
@@ -0,0 +1,18 @@
+# Codewars - Kata Solution
+# GRASSHOPPER  COMBINE STRINGS (8 kyu)
+
+# Instructions
+# Combine strings function
+# Create a function named (combine_names) that accepts two parameters (first and last name). The function should return the full name.
+# Example:
+# combine_names('James', 'Stevens')
+# returns:
+# 'James Stevens'
+
+# Solution
+def combine_names(a, b)
+  "#{a} #{b}"
+end  
+
+
+# codewars_id: 55f73f66d160f1f1db000059   |   21/11/2020 - 16:24:02
