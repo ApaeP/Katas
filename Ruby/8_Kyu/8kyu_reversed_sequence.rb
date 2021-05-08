d49d900e4b0556418a8caf8548c7c00528e9ed87
Paul Portier <paulportier@gmail.com>
2021-05-08 14:26:04 +0200

    Last resolved : (Reversed sequence 8kyu)

---
+++ Ruby/8_Kyu/8kyu_reversed_sequence.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# REVERSED SEQUENCE (8 kyu)
+
+# Instructions
+# Build a function that returns an array of integers from n to 1 where n>0.
+# Example : n=5 --> [5,4,3,2,1]
+
+# Solution
+def reverse_seq(n)
+  (1..n).to_a.reverse
+end
+
+
+# codewars_id: 5a00e05cc374cb34d100000d   |   30/10/2020 - 01:17:36
