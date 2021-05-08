564f93cdbc4d4b86ea73fadfd0b089aa6153c196
Paul Portier <paulportier@gmail.com>
2021-05-08 14:33:00 +0200

    Last resolved : (Function 1  hello world 8kyu)

---
+++ Ruby/8_Kyu/8kyu_function_1_hello_world.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# FUNCTION 1  HELLO WORLD (8 kyu)
+
+# Instructions
+# Description:
+# Make a simple function called greet that returns the most-famous "hello world!".
+# Style Points
+# Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+# Solution
+def greet; "hello world!" end
+
+
+# codewars_id: 523b4ff7adca849afe000035   |   29/10/2020 - 01:44:20
