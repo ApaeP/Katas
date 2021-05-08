93160159194ba5578d43957d14860336c0ac34a8
Paul Portier <paulportier@gmail.com>
2021-05-08 14:33:09 +0200

    Last resolved : (Grasshopper  Messi Goals 8kyu)

---
+++ Ruby/8_Kyu/8kyu_grasshopper_messi_goals.rb
@@ -0,0 +1,25 @@
+# Codewars - Kata Solution
+# GRASSHOPPER  MESSI GOALS (8 kyu)
+
+# Instructions
+# Messi's Goal Total
+# Use variables to find the sum of the goals Messi scored in 3 competitions
+# Information
+# Messi goal scoring statistics:
+# Competition Goals
+# La Liga 43
+# Champions League 10
+# Copa del Rey 5
+# Task
+# Create these three variables and store the appropriate values using the table above:
+# la_liga_goals
+# champions_league_goals
+# copa_del_rey_goals
+# Create a fourth variable named total_goals that stores the sum of all of Messi's goals for this year.
+
+# Solution
+la_liga_goals, champions_league_goals, copa_del_rey_goals = 43, 10, 5
+total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
+
+
+# codewars_id: 55ca77fa094a2af31f00002a   |   29/10/2020 - 00:31:46
