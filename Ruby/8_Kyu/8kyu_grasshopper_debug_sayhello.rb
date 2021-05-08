59625822feec61a87e37361623b1ba701255fc11
Paul Portier <paulportier@gmail.com>
2021-05-08 14:33:12 +0200

    Last resolved : (Grasshopper  Debug sayHello 8kyu)

---
+++ Ruby/8_Kyu/8kyu_grasshopper_debug_sayhello.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# GRASSHOPPER  DEBUG SAYHELLO (8 kyu)
+
+# Instructions
+# Debugging sayHello function
+# The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+# Example output:
+# Hello, Mr. Spock
+
+# Solution
+def say_hello(name)
+  "Hello, #{name}"
+end
+
+
+# codewars_id: 5625618b1fe21ab49f00001f   |   29/10/2020 - 00:12:28
