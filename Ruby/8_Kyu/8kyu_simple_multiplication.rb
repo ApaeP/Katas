c6c20fb46b90a003b81b31e03099859b34a3876c
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:08 +0200

    Last resolved : (Simple multiplication 8kyu)

---
+++ Ruby/8_Kyu/8kyu_simple_multiplication.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# SIMPLE MULTIPLICATION (8 kyu)
+
+# Instructions
+# This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+# Solution
+def simple_multiplication(num)
+  num.even? ? num*8 : num*9
+end
+
+
+# codewars_id: 583710ccaa6717322c000105   |   01/11/2020 - 20:51:18
