8c11d569cffbc2fddefbd78dc0846bea7309f69a
Paul Portier <paulportier@gmail.com>
2021-05-08 14:19:50 +0200

    Last resolved : (Flatten and sort an array 7kyu)

---
+++ Ruby/7_Kyu/7kyu_flatten_and_sort_an_array.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# FLATTEN AND SORT AN ARRAY (7 kyu)
+
+# Instructions
+# Challenge:
+# Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+# Example:
+# Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+# Solution
+def flatten_and_sort(arr)
+  arr.flatten.sort
+end
+
+
+# codewars_id: 57ee99a16c8df7b02d00045f   |   31/12/2020 - 03:12:24
