876690c0f7b5dc724cb331b2a2d929684ae737d8
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:30 +0200

    Last resolved : (Function 3  multiplying two numbers 8kyu)

---
+++ Ruby/8_Kyu/8kyu_function_3_multiplying_two_numbers.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# FUNCTION 3  MULTIPLYING TWO NUMBERS (8 kyu)
+
+# Instructions
+# Implement a function which multiplies two numbers.
+
+# Solution
+def multiply(a, b)
+  a*b
+end
+
+
+# codewars_id: 523b66342d0c301ae400003b   |   03/11/2020 - 03:46:03
