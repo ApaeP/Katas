24a3e6c1b608532a6bb7412ee800291c7b56d85c
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:51 +0200

    Last resolved : (Convert a Boolean to a String 8kyu)

---
+++ Ruby/8_Kyu/8kyu_convert_a_boolean_to_a_string.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# CONVERT A BOOLEAN TO A STRING (8 kyu)
+
+# Instructions
+# Implement a function which convert the given boolean value into its string representation.
+# Note: Only valid inputs will be given.
+
+# Solution
+def boolean_to_string(b)
+  b.to_s
+end
+
+
+# codewars_id: 551b4501ac0447318f0009cd   |   03/11/2020 - 01:21:34
