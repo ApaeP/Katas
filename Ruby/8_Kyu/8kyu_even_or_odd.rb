e0cc9ffa518f20a8386a5cf02eba4331c2c135ed
Paul Portier <paulportier@gmail.com>
2021-05-08 14:34:11 +0200

    Last resolved : (Even or Odd 8kyu)

---
+++ Ruby/8_Kyu/8kyu_even_or_odd.rb
@@ -0,0 +1,17 @@
+# Codewars - Kata Solution
+# EVEN OR ODD (8 kyu)
+
+# Instructions
+# Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+# Solution
+def even_or_odd(number)
+if number.odd?
+return "Odd"
+  else
+  return "Even"
+  end
+end
+
+
+# codewars_id: 53da3dbb4a5168369a0000fe   |   01/11/2019 - 18:08:29
