42a73ca15379ff8cdafc8f4fa3bc916fc22dcd67
Paul Portier <paulportier@gmail.com>
2021-05-08 14:32:23 +0200

    Last resolved : (Convert number to reversed array of digits 8kyu)

---
+++ Ruby/8_Kyu/8kyu_convert_number_to_reversed_array_of_digits.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# CONVERT NUMBER TO REVERSED ARRAY OF DIGITS (8 kyu)
+
+# Instructions
+# Convert number to reversed array of digits
+# Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+# Example:
+# 348597 => [7,9,5,8,4,3]
+
+# Solution
+def digitize(n)
+  n.digits
+end
+
+
+# codewars_id: 5583090cbe83f4fd8c000051   |   30/10/2020 - 01:08:25
