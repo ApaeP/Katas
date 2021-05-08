1409ac2e244c59ed0f89ae5564882e624fd17663
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:36 +0200

    Last resolved : (Grasshopper  Personalized Message 8kyu)

---
+++ Ruby/8_Kyu/8kyu_grasshopper_personalized_message.rb
@@ -0,0 +1,17 @@
+# Codewars - Kata Solution
+# GRASSHOPPER  PERSONALIZED MESSAGE (8 kyu)
+
+# Instructions
+# Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+# Use conditionals to return the proper message:
+# case return
+# name equals owner 'Hello boss'
+# otherwise 'Hello guest'
+
+# Solution
+def greet(n,o) 
+  "Hello #{n==o ? "boss":"guest"}"
+end
+
+
+# codewars_id: 5772da22b89313a4d50012f7   |   30/10/2020 - 04:26:43
