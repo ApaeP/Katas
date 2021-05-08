3f92216a2211e50ef7d9274973fd8cb068a77486
Paul Portier <paulportier@gmail.com>
2021-05-08 14:32:28 +0200

    Last resolved : (Opposite number 8kyu)

---
+++ Ruby/8_Kyu/8kyu_opposite_number.rb
@@ -0,0 +1,17 @@
+# Codewars - Kata Solution
+# OPPOSITE NUMBER (8 kyu)
+
+# Instructions
+# Very simple, given a number, find its opposite.
+# Examples:
+# 1: -1
+# 14: -14
+# -34: 34
+
+# Solution
+def opposite(n)
+  -n
+end
+
+
+# codewars_id: 56dec885c54a926dcd001095   |   30/10/2020 - 00:52:44
