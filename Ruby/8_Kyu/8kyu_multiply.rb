53f94e0f6eb7d89acbedfe644ce8be0198cbf788
Paul Portier <paulportier@gmail.com>
2021-05-08 14:34:12 +0200

    Last resolved : (Multiply 8kyu)

---
+++ Ruby/8_Kyu/8kyu_multiply.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# MULTIPLY (8 kyu)
+
+# Instructions
+# This code does not execute properly. Try to figure out why.
+
+# Solution
+def multiply(a, b)
+  a * b
+end
+
+
+# codewars_id: 50654ddff44f800200000004   |   01/11/2019 - 17:59:11
