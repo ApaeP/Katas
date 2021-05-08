04eb0ce4cf3583804b90dc23632c273232007416
Paul Portier <paulportier@gmail.com>
2021-05-08 14:26:02 +0200

    Last resolved : (Incorrect division method 8kyu)

---
+++ Ruby/8_Kyu/8kyu_incorrect_division_method.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# INCORRECT DIVISION METHOD (8 kyu)
+
+# Instructions
+# This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+# Solution
+def divide_numbers x, y
+  x.to_f / y
+end
+
+
+# codewars_id: 54d1c59aba326343c80000e7   |   30/10/2020 - 01:22:55
