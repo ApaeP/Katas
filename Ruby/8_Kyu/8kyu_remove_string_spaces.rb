1447864f6a02cffcd8cf2c2284bd35943f4e575c
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:55 +0200

    Last resolved : (Remove String Spaces 8kyu)

---
+++ Ruby/8_Kyu/8kyu_remove_string_spaces.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# REMOVE STRING SPACES (8 kyu)
+
+# Instructions
+# Simple, remove the spaces from the string, then return the resultant string.
+
+# Solution
+def no_space(x)
+  x.tr(' ', '')
+end
+
+
+# codewars_id: 57eae20f5500ad98e50002c5   |   30/10/2020 - 02:27:20
