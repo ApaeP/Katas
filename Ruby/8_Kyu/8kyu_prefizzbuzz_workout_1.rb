5899ab82edb465f9e37821808023d1b10441b9f7
Paul Portier <paulportier@gmail.com>
2021-05-08 14:23:20 +0200

    Last resolved : (PreFizzBuzz Workout 1 8kyu)

---
+++ Ruby/8_Kyu/8kyu_prefizzbuzz_workout_1.rb
@@ -0,0 +1,21 @@
+# Codewars - Kata Solution
+# PREFIZZBUZZ WORKOUT 1 (8 kyu)
+
+# Instructions
+# This is the first step to understanding FizzBuzz.
+# Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+# Your expected output is an array of positive integers from 1 to n (inclusive).
+# Your job is to write an algorithm that gets you from the input to the output.
+
+# Solution
+def pre_fizz(n)
+  Array(1..n)
+end
+
+#What are the inputs to this function?
+
+
+#What are the expected outputs?
+
+
+# codewars_id: 569e09850a8e371ab200000b   |   21/11/2020 - 15:21:05
