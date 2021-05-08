38ecd7806d2daa389173590fd5af1aca2bec9981
Paul Portier <paulportier@gmail.com>
2021-05-08 14:19:43 +0200

    Last resolved : (Disemvowel Trolls 7kyu)

---
+++ Ruby/7_Kyu/7kyu_disemvowel_trolls.rb
@@ -0,0 +1,17 @@
+# Codewars - Kata Solution
+# DISEMVOWEL TROLLS (7 kyu)
+
+# Instructions
+# Trolls are attacking your comment section!
+# A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+# Your task is to write a function that takes a string and return a new string with all vowels removed.
+# For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+# Note: for this kata y isn't considered a vowel.
+
+# Solution
+def disemvowel(str)
+  str.tr('aeiouAEIOU', '')
+end
+
+
+# codewars_id: 52fba66badcd10859f00097e   |   02/01/2021 - 02:21:46
