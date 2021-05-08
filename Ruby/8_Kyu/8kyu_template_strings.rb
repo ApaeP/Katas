83ce25fc296428b6b9e8d3b2fc7a007f4709f7bf
Paul Portier <paulportier@gmail.com>
2021-05-08 14:22:46 +0200

    Last resolved : (Template Strings 8kyu)

---
+++ Ruby/8_Kyu/8kyu_template_strings.rb
@@ -0,0 +1,21 @@
+# Codewars - Kata Solution
+# TEMPLATE STRINGS (8 kyu)
+
+# Instructions
+# Template Strings
+# Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+# Task
+# Your task is to return the correct string using the Template String Feature.
+# Input
+# Two Strings, no validation is needed.
+# Output
+# You must output a string containing the two strings with the word ```' are '```
+# Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/template_strings
+
+# Solution
+def TempleStrings(obj, feature)
+  obj + ' are ' + feature
+end
+
+
+# codewars_id: 55a14f75ceda999ced000048   |   21/11/2020 - 19:11:23
