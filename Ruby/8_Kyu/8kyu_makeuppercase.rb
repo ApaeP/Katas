3174c9ae8cb70c8ac2342a166cd27e09339606dd
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:32 +0200

    Last resolved : (MakeUpperCase 8kyu)

---
+++ Ruby/8_Kyu/8kyu_makeuppercase.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# MAKEUPPERCASE (8 kyu)
+
+# Instructions
+# Write a function which converts the input string to uppercase.
+
+# Solution
+def make_upper_case(str)
+  str.upcase
+end
+
+
+# codewars_id: 57a0556c7cb1f31ab3000ad7   |   30/10/2020 - 04:48:11
