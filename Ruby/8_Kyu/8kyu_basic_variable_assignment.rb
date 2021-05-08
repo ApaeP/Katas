597dde2506f3bd7e28960cc0aca285499de3ebf4
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:53 +0200

    Last resolved : (Basic variable assignment 8kyu)

---
+++ Ruby/8_Kyu/8kyu_basic_variable_assignment.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# BASIC VARIABLE ASSIGNMENT (8 kyu)
+
+# Instructions
+# This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+# Solution
+a = "code"
+b = "wa.rs"
+name = a + b
+
+
+# codewars_id: 50ee6b0bdeab583673000025   |   03/11/2020 - 01:13:48
