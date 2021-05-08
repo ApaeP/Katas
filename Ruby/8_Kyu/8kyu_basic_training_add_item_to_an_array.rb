d4b19b5a71e12fbf2550b986d3ccb3d34b459680
Paul Portier <paulportier@gmail.com>
2021-05-08 14:22:48 +0200

    Last resolved : (Basic Training Add item to an Array 8kyu)

---
+++ Ruby/8_Kyu/8kyu_basic_training_add_item_to_an_array.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# BASIC TRAINING ADD ITEM TO AN ARRAY (8 kyu)
+
+# Instructions
+# Add the value "codewars" to the websites array.
+# After your code executes the websites array should == ["codewars"]
+# The websites array has already been defined for you using the following code:
+# websites = []
+
+# Solution
+websites << 'codewars'
+
+
+# codewars_id: 511f0fe64ae8683297000001   |   21/11/2020 - 19:00:22
