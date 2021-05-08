b182855061882fcd5d2a93c6b18ba73c82fec14c
Paul Portier <paulportier@gmail.com>
2021-05-08 14:20:17 +0200

    Last resolved : (Format a string of names like Bart Lisa  Maggie 6kyu)

---
+++ Ruby/6_Kyu/6kyu_format_a_string_of_names_like_bart_lisa_maggie.rb
@@ -0,0 +1,27 @@
+# Codewars - Kata Solution
+# FORMAT A STRING OF NAMES LIKE BART LISA  MAGGIE (6 kyu)
+
+# Instructions
+# Given: an array containing hashes of names
+# Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+# Example:
+# list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+# # returns 'Bart, Lisa & Maggie'
+
+# list([ {name: 'Bart'}, {name: 'Lisa'} ])
+# # returns 'Bart & Lisa'
+
+# list([ {name: 'Bart'} ])
+# # returns 'Bart'
+
+# list([])
+# # returns ''
+# Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
+
+# Solution
+def list names
+  "#{"#{names[0..-2].map { |e| e[:name] }.join(', ')} & " if names.size > 1}#{names.last[:name]}" rescue ""
+end
+
+
+# codewars_id: 53368a47e38700bd8300030d   |   19/12/2020 - 02:16:37
