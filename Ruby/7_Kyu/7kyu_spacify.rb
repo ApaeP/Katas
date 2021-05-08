bee1de180d9261cbb1c86faa9ffbacc50cccca60
Paul Portier <paulportier@gmail.com>
2021-05-08 14:19:50 +0200

    Last resolved : (Spacify 7kyu)

---
+++ Ruby/7_Kyu/7kyu_spacify.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# SPACIFY (7 kyu)
+
+# Instructions
+# Modify the spacify function so that it returns the given string with spaces inserted between each character.
+# spacify("hello world") # returns "h e l l o   w o r l d"
+
+# Solution
+def spacify(str)
+  str.chars.join(' ')
+end
+
+
+# codewars_id: 57f8ee485cae443c4d000127   |   31/12/2020 - 02:47:44
