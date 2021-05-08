ac3f12f5cd525b8a31746d2bbd5b75c460165a16
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:44 +0200

    Last resolved : (get character from ASCII Value 8kyu)

---
+++ Ruby/8_Kyu/8kyu_get_character_from_ascii_value.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# GET CHARACTER FROM ASCII VALUE (8 kyu)
+
+# Instructions
+# Write a function which takes a number and returns the corresponding ASCII char for that value.
+# Example:
+# get_char(65) # => 'A'
+# For ASCII table, you can refer to http://www.asciitable.com/
+
+# Solution
+def getChar(c)
+  c.chr
+end
+
+
+# codewars_id: 55ad04714f0b468e8200001c   |   30/10/2020 - 03:20:35
