434f872e901d7577fad3b9aaba169818207bd936
Paul Portier <paulportier@gmail.com>
2021-05-08 14:33:28 +0200

    Last resolved : (Returning Strings 8kyu)

---
+++ Ruby/8_Kyu/8kyu_returning_strings.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# RETURNING STRINGS (8 kyu)
+
+# Instructions
+# Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+# [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+# Solution
+def greet(name)
+  "Hello, #{name} how are you doing today?"
+end
+
+
+# codewars_id: 55a70521798b14d4750000a4   |   28/10/2020 - 22:24:00
