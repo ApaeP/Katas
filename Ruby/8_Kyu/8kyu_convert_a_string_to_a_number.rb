f09bf74b9dbb13403c315e6db8876cac39b773d5
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:26 +0200

    Last resolved : (Convert a String to a Number 8kyu)

---
+++ Ruby/8_Kyu/8kyu_convert_a_string_to_a_number.rb
@@ -0,0 +1,21 @@
+# Codewars - Kata Solution
+# CONVERT A STRING TO A NUMBER (8 kyu)
+
+# Instructions
+# Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+# Description
+# We need a function that can transform a string into a number. What ways of achieving this do you know?
+# Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+# Examples
+# "1234" --> 1234
+# "605"  --> 605
+# "1405" --> 1405
+# "-7" --> -7
+
+# Solution
+def string_to_number(s)
+  s.to_i
+end
+
+
+# codewars_id: 544675c6f971f7399a000e79   |   30/10/2020 - 14:27:42
