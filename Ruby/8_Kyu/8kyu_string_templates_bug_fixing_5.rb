5f0a89fb460a535726e3330aa13645b74f220bfe
Paul Portier <paulportier@gmail.com>
2021-05-08 14:23:11 +0200

    Last resolved : (String Templates  Bug Fixing 5 8kyu)

---
+++ Ruby/8_Kyu/8kyu_string_templates_bug_fixing_5.rb
@@ -0,0 +1,15 @@
+# Codewars - Kata Solution
+# STRING TEMPLATES  BUG FIXING 5 (8 kyu)
+
+# Instructions
+# Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+# ◄PREVIOUS KATA
+# NEXT KATA►
+
+# Solution
+def build_string(*args)
+  "I like #{args.join(', ')}!"
+end
+
+
+# codewars_id: 55c90cad4b0fe31a7200001f   |   21/11/2020 - 16:22:25
