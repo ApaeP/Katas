d60b2f89529a3aa1be125f5790ceeb7600cb96f7
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:05 +0200

    Last resolved : (Enumerable Magic 18  All Items Except the First 8kyu)

---
+++ Ruby/8_Kyu/8kyu_enumerable_magic_18_all_items_except_the_first.rb
@@ -0,0 +1,15 @@
+# Codewars - Kata Solution
+# ENUMERABLE MAGIC 18  ALL ITEMS EXCEPT THE FIRST (8 kyu)
+
+# Instructions
+# Create a method drop that accepts a list, and a number n, and returns a copy of the list that skips the first n elements.
+# If you need help, here's a reference:
+# http://www.rubycuts.com/enum-drop
+
+# Solution
+def drop list, n
+  list.drop(n)
+end
+
+
+# codewars_id: 545aefb461aa4c3638001177   |   07/11/2020 - 01:15:16
