09088b27bcf6992fe523fcf2287b8c5fc9cfa0bd
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:09 +0200

    Last resolved : (Enumerable Magic 23  Filter Array by Regex 8kyu)

---
+++ Ruby/8_Kyu/8kyu_enumerable_magic_23_filter_array_by_regex.rb
@@ -0,0 +1,15 @@
+# Codewars - Kata Solution
+# ENUMERABLE MAGIC 23  FILTER ARRAY BY REGEX (8 kyu)
+
+# Instructions
+# Create a method grep that accepts a list and a regexp pattern, and returns a list of elements which match the pattern.
+# If you need help, here's a reference:
+# http://www.rubycuts.com/enum-grep
+
+# Solution
+def grep list, regexp
+  list.grep(regexp)
+end
+
+
+# codewars_id: 545af88285166a44e700123a   |   07/11/2020 - 01:10:38
