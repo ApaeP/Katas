21bfc6ee146f9e3c3cc702b2cfa4af5cc5ad6db5
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:59 +0200

    Last resolved : (Beginner  Lost Without a Map 8kyu)

---
+++ Ruby/8_Kyu/8kyu_beginner_lost_without_a_map.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# BEGINNER  LOST WITHOUT A MAP (8 kyu)
+
+# Instructions
+# Given an array of integers, return a new array with each value doubled.
+# For example:
+# [1, 2, 3] --> [2, 4, 6]
+# For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+
+# Solution
+def maps(x)
+  x.map { |i| i * 2 }
+end
+
+
+# codewars_id: 57f781872e3d8ca2a000007e   |   30/10/2020 - 01:33:17
