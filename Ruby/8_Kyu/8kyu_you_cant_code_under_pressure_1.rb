c2c59a39b5543ebcbb4b6644568939eda90fac92
Paul Portier <paulportier@gmail.com>
2021-05-08 14:33:25 +0200

    Last resolved : (You Cant Code Under Pressure 1 8kyu)

---
+++ Ruby/8_Kyu/8kyu_you_cant_code_under_pressure_1.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# YOU CANT CODE UNDER PRESSURE 1 (8 kyu)
+
+# Instructions
+# Code as fast as you can! You need to double the integer and return it.
+
+# Solution
+def double_integer(i)
+  i*2
+end
+
+
+# codewars_id: 53ee5429ba190077850011d4   |   28/10/2020 - 22:53:49
