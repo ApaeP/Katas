b1c7152ffe4e81209e07eb4e7ba6010809020825
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:32 +0200

    Last resolved : (Reverse List Order 8kyu)

---
+++ Ruby/8_Kyu/8kyu_reverse_list_order.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# REVERSE LIST ORDER (8 kyu)
+
+# Instructions
+# In this kata you will create a function that takes in a list and returns a list with the reverse order.
+# Examples
+# reverse_list([1,2,3,4]) == [4,3,2,1]
+# reverse_list([3,1,5,4]) == [4,5,1,3]
+
+# Solution
+def reverse_list list
+  list.reverse
+end
+
+
+# codewars_id: 53da6d8d112bd1a0dc00008b   |   03/11/2020 - 03:36:54
