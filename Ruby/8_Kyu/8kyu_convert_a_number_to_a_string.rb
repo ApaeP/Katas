19379623321cf145bb192a98f47fd5207548e870
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:00 +0200

    Last resolved : (Convert a Number to a String 8kyu)

---
+++ Ruby/8_Kyu/8kyu_convert_a_number_to_a_string.rb
@@ -0,0 +1,17 @@
+# Codewars - Kata Solution
+# CONVERT A NUMBER TO A STRING (8 kyu)
+
+# Instructions
+# We need a function that can transform a number into a string.
+# What ways of achieving this do you know?
+# Examples:
+# 123 --> "123"
+# 999 --> "999"
+
+# Solution
+def numberToString(n)
+  n.to_s
+end
+
+
+# codewars_id: 5265326f5fda8eb1160004c8   |   02/11/2020 - 05:36:26
