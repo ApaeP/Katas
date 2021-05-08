786ec316acf48b3e5c11203604011e5ab354fd36
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:30 +0200

    Last resolved : (Grasshopper  Terminal game move function 8kyu)

---
+++ Ruby/8_Kyu/8kyu_grasshopper_terminal_game_move_function.rb
@@ -0,0 +1,17 @@
+# Codewars - Kata Solution
+# GRASSHOPPER  TERMINAL GAME MOVE FUNCTION (8 kyu)
+
+# Instructions
+# Terminal game move function
+# In this game, the hero moves from left to right. The player rolls the die and moves the number of spaces indicated by the die two times.
+# Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+# Example:
+# move(3, 6) should equal 15
+
+# Solution
+def move (p, r)
+  p + r*2
+end
+
+
+# codewars_id: 563a631f7cbbc236cf0000c2   |   30/10/2020 - 04:52:32
