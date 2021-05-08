acb9699649d9f73929180505d9126ac8e26c3415
Paul Portier <paulportier@gmail.com>
2021-05-08 14:23:29 +0200

    Last resolved : (Grasshopper  Basic Function Fixer 8kyu)

---
+++ Ruby/8_Kyu/8kyu_grasshopper_basic_function_fixer.rb
@@ -0,0 +1,15 @@
+# Codewars - Kata Solution
+# GRASSHOPPER  BASIC FUNCTION FIXER (8 kyu)
+
+# Instructions
+# Fix the function
+# I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+# Can you help me fix the function?
+
+# Solution
+def addFive(num)
+  num + 5
+end
+
+
+# codewars_id: 56200d610758762fb0000002   |   21/11/2020 - 13:16:52
