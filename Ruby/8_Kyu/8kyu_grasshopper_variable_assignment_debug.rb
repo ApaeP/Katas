bbe40e7e07dba1ef4e3dd8557df1d6fad15b4810
Paul Portier <paulportier@gmail.com>
2021-05-08 14:23:11 +0200

    Last resolved : (Grasshopper  Variable Assignment Debug 8kyu)

---
+++ Ruby/8_Kyu/8kyu_grasshopper_variable_assignment_debug.rb
@@ -0,0 +1,15 @@
+# Codewars - Kata Solution
+# GRASSHOPPER  VARIABLE ASSIGNMENT DEBUG (8 kyu)
+
+# Instructions
+# Variable assignment
+# Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+# Solution
+a = "dev"
+b = "Lab"
+
+name = a + b
+
+
+# codewars_id: 5612e743cab69fec6d000077   |   21/11/2020 - 16:22:53
