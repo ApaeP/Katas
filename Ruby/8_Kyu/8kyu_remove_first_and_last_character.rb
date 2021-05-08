3fafa5bd317a46baae621a28ebd954c33ccd520e
Paul Portier <paulportier@gmail.com>
2021-05-08 14:32:20 +0200

    Last resolved : (Remove First and Last Character 8kyu)

---
+++ Ruby/8_Kyu/8kyu_remove_first_and_last_character.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# REMOVE FIRST AND LAST CHARACTER (8 kyu)
+
+# Instructions
+# It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+# Solution
+def remove_char(s)
+  s[1..-2]
+end
+
+
+# codewars_id: 56bc28ad5bdaeb48760009b0   |   30/10/2020 - 01:11:30
