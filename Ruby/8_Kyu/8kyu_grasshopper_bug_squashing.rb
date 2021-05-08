7bcea2ad04afe0390da0f1e9ecf269480f91ce46
Paul Portier <paulportier@gmail.com>
2021-05-08 14:23:57 +0200

    Last resolved : (Grasshopper  Bug Squashing 8kyu)

---
+++ Ruby/8_Kyu/8kyu_grasshopper_bug_squashing.rb
@@ -0,0 +1,23 @@
+# Codewars - Kata Solution
+# GRASSHOPPER  BUG SQUASHING (8 kyu)
+
+# Instructions
+# Terminal game bug squashing
+# You are creating a text-based terminal version of your favorite board game. In the board game, each turn has six steps that must happen in this order: roll the dice, move, combat, get coins, buy more health, and print status.
+# You are using a library that already has the functions below. Create a function named main that calls the functions in the proper order.
+# - `combat`
+# - `buy_health`
+# - `get_coins`
+# - `print_status`
+# - `roll_dice`
+# - `move`
+
+# Solution
+health, position, coins = 100, 0, 0
+
+def main
+  roll_dice; move; combat; get_coins; buy_health; print_status
+end
+
+
+# codewars_id: 56214b6864fe8813f1000019   |   07/11/2020 - 01:21:10
