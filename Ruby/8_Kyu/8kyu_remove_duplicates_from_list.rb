dda0efe177fcb86cdb57ef18024033f0f00de029
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:24 +0200

    Last resolved : (Remove duplicates from list 8kyu)

---
+++ Ruby/8_Kyu/8kyu_remove_duplicates_from_list.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# REMOVE DUPLICATES FROM LIST (8 kyu)
+
+# Instructions
+# Define a function that removes duplicates from an array of numbers and returns it as a result.
+# The order of the sequence has to stay the same.
+
+# Solution
+def distinct(seq)
+  seq.uniq
+end
+
+
+# codewars_id: 57a5b0dfcf1fa526bb000118   |   03/11/2020 - 04:26:28
