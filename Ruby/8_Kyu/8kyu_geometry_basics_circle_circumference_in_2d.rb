4e2e0ecda431e98043ae09da43e89d16ef631593
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:27 +0200

    Last resolved : (Geometry Basics Circle Circumference in 2D 8kyu)

---
+++ Ruby/8_Kyu/8kyu_geometry_basics_circle_circumference_in_2d.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# GEOMETRY BASICS CIRCLE CIRCUMFERENCE IN 2D (8 kyu)
+
+# Instructions
+# This series of katas will introduce you to basics of doing geometry with computers.
+# Point objects have x, y attributes. Circle objects have center which is a Point, and radius, which is a number.
+# Write a function calculating circumference of a Circle.
+# Tests round answers to 6 decimal places.
+
+# Solution
+def circle_circumference(circle)
+  Math::PI * circle.radius * 2
+end
+
+
+# codewars_id: 58e43389acfd3e81d5000a88   |   03/11/2020 - 04:04:53
