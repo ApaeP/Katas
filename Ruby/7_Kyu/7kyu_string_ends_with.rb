d048ec6dc4b4cdf5bff35da49b327384ed60d09f
Paul Portier <paulportier@gmail.com>
2021-05-08 14:22:21 +0200

    Last resolved : (String ends with 7kyu)

---
+++ Ruby/7_Kyu/7kyu_string_ends_with.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# STRING ENDS WITH (7 kyu)
+
+# Instructions
+# Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+# Examples:
+# solution('abc', 'bc') // returns true
+# solution('abc', 'd') // returns false
+
+# Solution
+def solution(str, ending)
+  str.end_with?(ending)
+end
+
+
+# codewars_id: 51f2d1cafc9c0f745c00037d   |   23/11/2020 - 13:28:26
