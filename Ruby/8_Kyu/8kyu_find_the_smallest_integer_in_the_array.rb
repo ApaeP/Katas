d14b7ae1281964e58371d8dc09c09d0c8aeb46d2
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:09 +0200

    Last resolved : (Find the smallest integer in the array 8kyu)

---
+++ Ruby/8_Kyu/8kyu_find_the_smallest_integer_in_the_array.rb
@@ -0,0 +1,17 @@
+# Codewars - Kata Solution
+# FIND THE SMALLEST INTEGER IN THE ARRAY (8 kyu)
+
+# Instructions
+# Given an array of integers your solution should find the smallest integer.
+# For example:
+# Given [34, 15, 88, 2] your solution will return 2
+# Given [34, -345, -1, 100] your solution will return -345
+# You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+# Solution
+def find_smallest_int(arr)
+  arr.min
+end
+
+
+# codewars_id: 55a2d7ebe362935a210000b2   |   01/11/2020 - 20:50:37
