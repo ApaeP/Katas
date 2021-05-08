aca6d6200818ffc5b54d3c6712c7d29fcb81abca
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:23 +0200

    Last resolved : (Get Nth Even Number 8kyu)

---
+++ Ruby/8_Kyu/8kyu_get_nth_even_number.rb
@@ -0,0 +1,19 @@
+# Codewars - Kata Solution
+# GET NTH EVEN NUMBER (8 kyu)
+
+# Instructions
+# Return the Nth Even Number
+# Example(Input --> Output)
+# 1 --> 0 (the first even number is 0)
+# 3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+# 100 --> 198
+# 1298734 --> 2597466
+# The input will not be 0.
+
+# Solution
+def nth_even(n)
+  2*(n-1)
+end
+
+
+# codewars_id: 5933a1f8552bc2750a0000ed   |   30/10/2020 - 18:57:15
