4dc6438f1cbc19501dcfabea5bf4ef1c2089874c
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:03 +0200

    Last resolved : (Count Odd Numbers below n 8kyu)

---
+++ Ruby/8_Kyu/8kyu_count_odd_numbers_below_n.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# COUNT ODD NUMBERS BELOW N (8 kyu)
+
+# Instructions
+# Given a number n, return the number of positive odd numbers below n, EASY!
+# oddCount(7) //=> 3, i.e [1, 3, 5]
+# oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+# Expect large Inputs!
+
+# Solution
+def oddCount n 
+  n / 2
+end
+
+
+# codewars_id: 59342039eb450e39970000a6   |   01/11/2020 - 21:28:04
