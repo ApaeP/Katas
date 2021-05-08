10c68581a4e13feb8ee29043a05fd33bd0a6c21d
Paul Portier <paulportier@gmail.com>
2021-05-08 14:32:49 +0200

    Last resolved : (You only need one  Beginner 8kyu)

---
+++ Ruby/8_Kyu/8kyu_you_only_need_one_beginner.rb
@@ -0,0 +1,15 @@
+# Codewars - Kata Solution
+# YOU ONLY NEED ONE  BEGINNER (8 kyu)
+
+# Instructions
+# You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+# Array can contain numbers or strings. X can be either.
+# Return true if the array contains the value, false if not.
+
+# Solution
+def check(arr,element)
+  arr.include?(element)
+end
+
+
+# codewars_id: 57cc975ed542d3148f00015b   |   29/10/2020 - 02:35:09
