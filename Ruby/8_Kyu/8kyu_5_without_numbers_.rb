bc6200b1dd992e45a8a54e2f07061605e8f2346e
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:49 +0200

    Last resolved : (5 without numbers  8kyu)

---
+++ Ruby/8_Kyu/8kyu_5_without_numbers_.rb
@@ -0,0 +1,15 @@
+# Codewars - Kata Solution
+# 5 WITHOUT NUMBERS  (8 kyu)
+
+# Instructions
+# Write a function that always returns 5
+# Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+# Good luck :)
+
+# Solution
+def unusual_five
+  '.....'.length
+end  
+
+
+# codewars_id: 59441520102eaa25260000bf   |   30/10/2020 - 03:01:11
