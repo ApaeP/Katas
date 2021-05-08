5e51cd6c9ccacc7c7884443403789da16424771e
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:35 +0200

    Last resolved : (Enumerable Magic 3  Does My List Include This 8kyu)

---
+++ Ruby/8_Kyu/8kyu_enumerable_magic_3_does_my_list_include_this.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# ENUMERABLE MAGIC 3  DOES MY LIST INCLUDE THIS (8 kyu)
+
+# Instructions
+# Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+# If you need help, here's some reference material: http://www.rubycuts.com/enum-include
+
+# Solution
+def include? array, item
+  array.include?(item)
+end
+
+
+# codewars_id: 545991b4cbae2a5fda000158   |   03/11/2020 - 03:28:29
