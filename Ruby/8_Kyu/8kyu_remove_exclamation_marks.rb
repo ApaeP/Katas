49ccde875859d600ac428b1c97881e96dd24b195
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:43 +0200

    Last resolved : (Remove exclamation marks 8kyu)

---
+++ Ruby/8_Kyu/8kyu_remove_exclamation_marks.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# REMOVE EXCLAMATION MARKS (8 kyu)
+
+# Instructions
+# Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+# Solution
+def remove_exclamation_marks(s)
+  s.gsub('!', '')
+end
+
+
+# codewars_id: 57a0885cbb9944e24c00008e   |   03/11/2020 - 02:44:10
