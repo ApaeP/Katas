71e74849703cac2707f482175703920ed8ec6e3d
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:27 +0200

    Last resolved : (Third Angle of a Triangle 8kyu)

---
+++ Ruby/8_Kyu/8kyu_third_angle_of_a_triangle.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# THIRD ANGLE OF A TRIANGLE (8 kyu)
+
+# Instructions
+# You are given two interior angles (in degrees) of a triangle.
+# Write a function to return the 3rd.
+# Note: only positive integers will be tested.
+# https://en.wikipedia.org/wiki/Triangle
+
+# Solution
+def other_angle(a, b)
+  180 - (a+b)
+end
+
+
+# codewars_id: 5a023c426975981341000014   |   30/10/2020 - 14:26:58
