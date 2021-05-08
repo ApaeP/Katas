7e053c30bc48e8dbec05d1639ca593ce83fc6a89
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:16 +0200

    Last resolved : (Jennys secret message 8kyu)

---
+++ Ruby/8_Kyu/8kyu_jennys_secret_message.rb
@@ -0,0 +1,15 @@
+# Codewars - Kata Solution
+# JENNYS SECRET MESSAGE (8 kyu)
+
+# Instructions
+# Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+# Can you help her?
+
+# Solution
+def greet(name)
+  return "Hello, my love!" if name == "Johnny"
+  "Hello, #{name}!"
+end
+
+
+# codewars_id: 55225023e1be1ec8bc000390   |   03/11/2020 - 19:00:49
