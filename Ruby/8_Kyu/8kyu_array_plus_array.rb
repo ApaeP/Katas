6c74c6e54f3d047f285c85fdc8b817b86cfc6699
Paul Portier <paulportier@gmail.com>
2021-05-08 14:33:28 +0200

    Last resolved : (Array plus array 8kyu)

---
+++ Ruby/8_Kyu/8kyu_array_plus_array.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# ARRAY PLUS ARRAY (8 kyu)
+
+# Instructions
+# I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+# P.S. Each array includes only integer numbers. Output is a number too.
+
+# Solution
+def array_plus_array(arr1, arr2)
+  (arr1 + arr2).sum
+end
+
+
+# codewars_id: 5a2be17aee1aaefe2a000151   |   28/10/2020 - 22:23:13
