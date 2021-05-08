33b816ad54d3d28017bc1cc2c649daca79820f06
Paul Portier <paulportier@gmail.com>
2021-05-08 14:23:30 +0200

    Last resolved : (Unfinished Loop  Bug Fixing 1 8kyu)

---
+++ Ruby/8_Kyu/8kyu_unfinished_loop_bug_fixing_1.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# UNFINISHED LOOP  BUG FIXING 1 (8 kyu)
+
+# Instructions
+# Unfinished Loop - Bug Fixing #1
+# Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+# Solution
+def create_array(n)
+  Array.new(n) { |i| i+1}
+end
+
+
+# codewars_id: 55c28f7304e3eaebef0000da   |   21/11/2020 - 13:16:13
