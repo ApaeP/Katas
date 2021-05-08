4c0963f58fd3765b829c3acf86d2f7b92a22267a
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:17 +0200

    Last resolved : (Convert boolean values to strings Yes or No 8kyu)

---
+++ Ruby/8_Kyu/8kyu_convert_boolean_values_to_strings_yes_or_no.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# CONVERT BOOLEAN VALUES TO STRINGS YES OR NO (8 kyu)
+
+# Instructions
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+# Solution
+def bool_to_word bool
+  bool ? 'Yes' : 'No'
+end
+
+
+# codewars_id: 53369039d7ab3ac506000467   |   01/11/2020 - 20:17:05
