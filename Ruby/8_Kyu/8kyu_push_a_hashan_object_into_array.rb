6031f53afac6a4a46f3ca92b3782bab6e77e14fc
Paul Portier <paulportier@gmail.com>
2021-05-08 14:22:36 +0200

    Last resolved : (Push a hashan object into array 8kyu)

---
+++ Ruby/8_Kyu/8kyu_push_a_hashan_object_into_array.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# PUSH A HASHAN OBJECT INTO ARRAY (8 kyu)
+
+# Instructions
+# You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+
+# Solution
+#items = Array.new
+#items.push (:a => "b", :c => "d")
+items = [{:a => "b", :c => "d"}]
+
+
+# codewars_id: 527b3cd0492b6b15250060af   |   21/11/2020 - 19:59:17
