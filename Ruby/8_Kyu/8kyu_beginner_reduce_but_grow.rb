1c002c11bce871002adea55aa4e80a0000fc2876
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:06 +0200

    Last resolved : (Beginner  Reduce but Grow 8kyu)

---
+++ Ruby/8_Kyu/8kyu_beginner_reduce_but_grow.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# BEGINNER  REDUCE BUT GROW (8 kyu)
+
+# Instructions
+# Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+# [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+# Solution
+def grow(x) 
+  x.inject(:*)
+end
+
+
+# codewars_id: 57f780909f7e8e3183000078   |   01/11/2020 - 20:52:39
