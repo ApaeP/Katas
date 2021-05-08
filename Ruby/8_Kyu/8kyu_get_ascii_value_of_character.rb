a38679972cfddf76af8bbb184d0cf9aeef313996
Paul Portier <paulportier@gmail.com>
2021-05-08 14:32:37 +0200

    Last resolved : (get ascii value of character 8kyu)

---
+++ Ruby/8_Kyu/8kyu_get_ascii_value_of_character.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# GET ASCII VALUE OF CHARACTER (8 kyu)
+
+# Instructions
+# Get ASCII value of a character.
+# For the ASCII table you can refer to http://www.asciitable.com/
+
+# Solution
+def getASCII(c)
+  c.ord
+end
+
+
+# codewars_id: 55acfc59c3c23d230f00006d   |   29/10/2020 - 23:56:54
