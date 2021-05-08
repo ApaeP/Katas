98d317bd6638217cf7e21cec9d4ac24d76c3f323
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:41 +0200

    Last resolved : (Convert a string to an array 8kyu)

---
+++ Ruby/8_Kyu/8kyu_convert_a_string_to_an_array.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# CONVERT A STRING TO AN ARRAY (8 kyu)
+
+# Instructions
+# Write a function to split a string and convert it into an array of words. For example:
+# "Robin Singh" ==> ["Robin", "Singh"]
+
+# "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+# Solution
+def string_to_array(string)
+  string.split(' ')
+end
+
+
+# codewars_id: 57e76bc428d6fbc2d500036d   |   30/10/2020 - 03:45:17
