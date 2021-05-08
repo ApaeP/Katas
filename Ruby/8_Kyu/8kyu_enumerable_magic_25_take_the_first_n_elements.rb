82f9f6e1e197697df3eb0879cbe8f34c3f97566e
Paul Portier <paulportier@gmail.com>
2021-05-08 14:26:00 +0200

    Last resolved : (Enumerable Magic 25  Take the First N Elements 8kyu)

---
+++ Ruby/8_Kyu/8kyu_enumerable_magic_25_take_the_first_n_elements.rb
@@ -0,0 +1,15 @@
+# Codewars - Kata Solution
+# ENUMERABLE MAGIC 25  TAKE THE FIRST N ELEMENTS (8 kyu)
+
+# Instructions
+# Create a method take that accepts a list/array and a number n, and returns a list/array array of the first n elements from the list/array.
+# If you need help, here's a reference:
+# http://www.rubycuts.com/enum-take
+
+# Solution
+def take list, n
+  list.first(n)
+end
+
+
+# codewars_id: 545afd0761aa4c3055001386   |   30/10/2020 - 01:31:11
