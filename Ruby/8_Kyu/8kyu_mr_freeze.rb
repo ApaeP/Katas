23a29a6bb7783314d7377041c7046b668b906284
Paul Portier <paulportier@gmail.com>
2021-05-08 14:22:49 +0200

    Last resolved : (Mr Freeze 8kyu)

---
+++ Ruby/8_Kyu/8kyu_mr_freeze.rb
@@ -0,0 +1,11 @@
+# Codewars - Kata Solution
+# MR FREEZE (8 kyu)
+
+# Instructions
+# There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
+
+# Solution
+MrFreeze.freeze
+
+
+# codewars_id: 514a3996d22ce03198000003   |   21/11/2020 - 18:59:34
