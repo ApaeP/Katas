f70e7110605fd64aea7f10394e49915b559d8041
Paul Portier <paulportier@gmail.com>
2021-05-08 14:32:38 +0200

    Last resolved : (Number toString 8kyu)

---
+++ Ruby/8_Kyu/8kyu_number_tostring.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# NUMBER TOSTRING (8 kyu)
+
+# Instructions
+# The code gives an error!
+# a = some_number.toString
+# Fix it!
+
+# Solution
+a = 123.to_s
+
+
+# codewars_id: 53934feec44762736c00044b   |   29/10/2020 - 23:56:00
