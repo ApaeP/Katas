71d6eb546eac25597ef1bc310c47816903b941f6
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:57 +0200

    Last resolved : (What is between 8kyu)

---
+++ Ruby/8_Kyu/8kyu_what_is_between.rb
@@ -0,0 +1,17 @@
+# Codewars - Kata Solution
+# WHAT IS BETWEEN (8 kyu)
+
+# Instructions
+# Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+# For example:
+# a = 1
+# b = 4
+# --> [1, 2, 3, 4]
+
+# Solution
+def between(a, b)
+  (a..b).to_a
+end
+
+
+# codewars_id: 55ecd718f46fba02e5000029   |   30/10/2020 - 01:37:01
