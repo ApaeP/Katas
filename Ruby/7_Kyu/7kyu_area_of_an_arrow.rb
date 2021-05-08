4f7f69f97dff52b8a6b162b0e571f3b1baac68c3
Paul Portier <paulportier@gmail.com>
2021-05-08 14:20:04 +0200

    Last resolved : (Area of an arrow 7kyu)

---
+++ Ruby/7_Kyu/7kyu_area_of_an_arrow.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# AREA OF AN ARROW (7 kyu)
+
+# Instructions
+# Area of an arrow
+# An arrow is formed in a rectangle with sides a and b by joining the bottom corners to the midpoint of the top edge and the centre of the rectangle.
+# a and b are integers and > 0
+# Write a function which returns the area of the arrow.
+
+# Solution
+def arrow_area(a, b)
+  a * b / 4.0
+end
+
+
+# codewars_id: 589478160c0f8a40870000bc   |   30/12/2020 - 19:04:47
