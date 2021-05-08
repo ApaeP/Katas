b3ed07152d004835d0ab0721f84d3f607880644e
Paul Portier <paulportier@gmail.com>
2021-05-08 14:32:24 +0200

    Last resolved : (Reversed Strings 8kyu)

---
+++ Ruby/8_Kyu/8kyu_reversed_strings.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# REVERSED STRINGS (8 kyu)
+
+# Instructions
+# Complete the solution so that it reverses the string passed into it.
+# 'world'  =>  'dlrow'
+
+# Solution
+def solution(str)
+  str.reverse
+end
+
+
+# codewars_id: 5168bb5dfe9a00b126000018   |   30/10/2020 - 01:07:36
