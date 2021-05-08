34c0c403a24ac2457608fabb881930a9d0eaed7a
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:36 +0200

    Last resolved : (Super Duper Easy 8kyu)

---
+++ Ruby/8_Kyu/8kyu_super_duper_easy.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# SUPER DUPER EASY (8 kyu)
+
+# Instructions
+# Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+# Solution
+def problem x
+  x.is_a?(String) ? 'Error' : x*50+6
+end
+
+
+# codewars_id: 55a5bfaa756cfede78000026   |   03/11/2020 - 03:27:29
