f445bba2632feb728ed9e3bda407f55261bd87a8
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:50 +0200

    Last resolved : (Hex to Decimal 8kyu)

---
+++ Ruby/8_Kyu/8kyu_hex_to_decimal.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# HEX TO DECIMAL (8 kyu)
+
+# Instructions
+# Complete the function which converts hex number (given as a string) to a decimal number.
+
+# Solution
+def hex_to_dec(hex_string)
+  hex_string.hex
+end
+
+
+# codewars_id: 57a4d500e298a7952100035d   |   03/11/2020 - 01:23:43
