28d2b635f52791901100d3e5df82e5e9e54385fc
Paul Portier <paulportier@gmail.com>
2021-05-08 14:33:14 +0200

    Last resolved : (Grasshopper  Function syntax debugging 8kyu)

---
+++ Ruby/8_Kyu/8kyu_grasshopper_function_syntax_debugging.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# GRASSHOPPER  FUNCTION SYNTAX DEBUGGING (8 kyu)
+
+# Instructions
+# Grasshopper - Function syntax debugging
+# A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+# Solution
+def main(verb, noun)
+  verb + noun
+end
+
+
+# codewars_id: 56dae9dc54c0acd29d00109a   |   29/10/2020 - 00:10:00
