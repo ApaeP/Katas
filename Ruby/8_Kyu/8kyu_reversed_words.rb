6283c7c13b7e4c120d45bb8736a7cb5363fa067d
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:57 +0200

    Last resolved : (Reversed Words 8kyu)

---
+++ Ruby/8_Kyu/8kyu_reversed_words.rb
@@ -0,0 +1,15 @@
+# Codewars - Kata Solution
+# REVERSED WORDS (8 kyu)
+
+# Instructions
+# Complete the solution so that it reverses all of the words within the string passed in.
+# Example:
+# "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+# Solution
+def solution(s)
+  s.split(' ').reverse.join(' ')
+end
+
+
+# codewars_id: 51c8991dee245d7ddf00000e   |   03/11/2020 - 00:58:08
