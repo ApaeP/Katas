e492ca49d8dbee9a96c42edb784265dca13d713a
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:59 +0200

    Last resolved : (Bin to Decimal 8kyu)

---
+++ Ruby/8_Kyu/8kyu_bin_to_decimal.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# BIN TO DECIMAL (8 kyu)
+
+# Instructions
+# Complete the function which converts a binary number (given as a string) to a decimal number.
+
+# Solution
+def bin_to_dec(str)
+  str.to_i(2)
+end
+
+
+# codewars_id: 57a5c31ce298a7e6b7000334   |   02/11/2020 - 05:39:55
