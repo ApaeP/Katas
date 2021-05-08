b1423c85487795bbe9171e8ef2b09f6ae8ece109
Paul Portier <paulportier@gmail.com>
2021-05-08 14:23:08 +0200

    Last resolved : (Printing Array elements with Comma delimiters 8kyu)

---
+++ Ruby/8_Kyu/8kyu_printing_array_elements_with_comma_delimiters.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# PRINTING ARRAY ELEMENTS WITH COMMA DELIMITERS (8 kyu)
+
+# Instructions
+# Input: Array of elements
+# ["h","o","l","a"]
+# Output: String with comma delimited elements of the array in th same order.
+# "h,o,l,a"
+
+# Solution
+def printArray(a)
+  a.join(',')
+end
+
+
+# codewars_id: 56e2f59fb2ed128081001328   |   21/11/2020 - 16:25:57
