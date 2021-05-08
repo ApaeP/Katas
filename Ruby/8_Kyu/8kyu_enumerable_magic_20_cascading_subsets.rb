2f5d6240b06ddc658f2ef2bdac1539e9b63fb6be
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:05 +0200

    Last resolved : (Enumerable Magic 20  Cascading Subsets 8kyu)

---
+++ Ruby/8_Kyu/8kyu_enumerable_magic_20_cascading_subsets.rb
@@ -0,0 +1,22 @@
+# Codewars - Kata Solution
+# ENUMERABLE MAGIC 20  CASCADING SUBSETS (8 kyu)
+
+# Instructions
+# Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so:
+# each_cons([1,2,3,4], 2)
+#   #=> [[1,2], [2,3], [3,4]]
+
+# each_cons([1,2,3,4], 3)
+#   #=> [[1,2,3],[2,3,4]]
+#   
+# As you can see, the lists are cascading; ie, they overlap, but never out of order.
+
+# Solution
+def each_cons list, n
+  arr = []
+  list.each_cons(n) { |e| arr << e }
+  arr
+end
+
+
+# codewars_id: 545af3d185166a3dec001190   |   07/11/2020 - 01:14:42
