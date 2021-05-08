0630b4af8095f47414d5459b90ae67748dec4506
Paul Portier <paulportier@gmail.com>
2021-05-08 14:22:54 +0200

    Last resolved : (Whats wrong with these identifiers Retiredkyu)

---
+++ Ruby/Retired_Kyu/Retiredkyu_whats_wrong_with_these_identifiers.rb
@@ -0,0 +1,17 @@
+# Codewars - Kata Solution
+# WHATS WRONG WITH THESE IDENTIFIERS (Retired kyu)
+
+# Instructions
+# An identifier is simply a name...
+# Can you amend this object so that its properties comprise only vaild identifiers?
+
+# Solution
+Person = {
+  "1stname" => "John",
+  "second-name" => "Doe",
+  "email@ddress" => "john.doe@email.com",
+  "male.female" => "M"
+}
+
+
+# codewars_id: 56bb01de0e8b29de50000b19   |   21/11/2020 - 18:30:07
