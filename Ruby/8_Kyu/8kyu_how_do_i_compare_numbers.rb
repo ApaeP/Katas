0894442b1d31851f17b459f6412377aeec227424
Paul Portier <paulportier@gmail.com>
2021-05-08 14:32:48 +0200

    Last resolved : (How do I compare numbers 8kyu)

---
+++ Ruby/8_Kyu/8kyu_how_do_i_compare_numbers.rb
@@ -0,0 +1,19 @@
+# Codewars - Kata Solution
+# HOW DO I COMPARE NUMBERS (8 kyu)
+
+# Instructions
+# What could be easier than comparing integer numbers? However, the given piece of code doesn't recognize some of the special numbers for a reason to be found. Your task is to find the bug and eliminate it.
+
+# Solution
+def what_is(x)
+  if x == 42
+    'everything'
+  elsif x == 42 ** 42
+    'everything everythinged'
+  else
+    'nothing'
+  end
+end
+
+
+# codewars_id: 55d8618adfda93c89600012e   |   29/10/2020 - 02:37:09
