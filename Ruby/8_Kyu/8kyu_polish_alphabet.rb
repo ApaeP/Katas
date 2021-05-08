6306e2c5b17bc4caabdf966bdc4c78775c8381bc
Paul Portier <paulportier@gmail.com>
2021-05-08 14:32:43 +0200

    Last resolved : (Polish alphabet 8kyu)

---
+++ Ruby/8_Kyu/8kyu_polish_alphabet.rb
@@ -0,0 +1,26 @@
+# Codewars - Kata Solution
+# POLISH ALPHABET (8 kyu)
+
+# Instructions
+# There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+# Your task is to change the letters with diacritics:
+# ą -> a,
+# ć -> c,
+# ę -> e,
+# ł -> l,
+# ń -> n,
+# ó -> o,
+# ś -> s,
+# ź -> z,
+# ż -> z
+# and print out the string without the use of the Polish letters.
+# For example:
+# "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+# Solution
+def correct_polish_letters(s)
+  s.tr('ąćęłńóśźż', 'acelnoszz')
+end
+
+
+# codewars_id: 57ab2d6072292dbf7c000039   |   29/10/2020 - 23:42:54
