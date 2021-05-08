d42d784715fa1dae8c06fbcf96d0e93f4fc234bf
Paul Portier <paulportier@gmail.com>
2021-05-08 14:25:35 +0200

    Last resolved : (Surface Area and Volume of a Box 8kyu)

---
+++ Ruby/8_Kyu/8kyu_surface_area_and_volume_of_a_box.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# SURFACE AREA AND VOLUME OF A BOX (8 kyu)
+
+# Instructions
+# Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+# Solution
+def get_size(w,h,d)
+  [(2*w*h)+(2*h*d)+(2*d*w), w*h*d]
+end
+
+
+# codewars_id: 565f5825379664a26b00007c   |   30/10/2020 - 04:30:03
