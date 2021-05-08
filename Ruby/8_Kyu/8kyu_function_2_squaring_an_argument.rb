99fe01e5ed1584d7d8da1d49539ef92d9b33541b
Paul Portier <paulportier@gmail.com>
2021-05-08 14:24:42 +0200

    Last resolved : (Function 2  squaring an argument 8kyu)

---
+++ Ruby/8_Kyu/8kyu_function_2_squaring_an_argument.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# FUNCTION 2  SQUARING AN ARGUMENT (8 kyu)
+
+# Instructions
+# Now you have to write a function that takes an argument and returns the square of it.
+
+# Solution
+def square n
+  n**2
+end
+
+
+# codewars_id: 523b623152af8a30c6000027   |   03/11/2020 - 02:46:41
