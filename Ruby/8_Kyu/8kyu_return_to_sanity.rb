7b8e8912b1d555366646992e663c17201f612050
Paul Portier <paulportier@gmail.com>
2021-05-08 14:22:40 +0200

    Last resolved : (Return to Sanity 8kyu)

---
+++ Ruby/8_Kyu/8kyu_return_to_sanity.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# RETURN TO SANITY (8 kyu)
+
+# Instructions
+# This function should return an object, but it's not doing what's intended. What's wrong?
+
+# Solution
+def mystery()
+  {:sanity => 'Hello'}
+
+end
+
+
+# codewars_id: 514a7ac1a33775cbb500001e   |   21/11/2020 - 19:21:01
