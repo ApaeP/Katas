272cb84211e2858b424fda77d936c23abab7be59
Paul Portier <paulportier@gmail.com>
2021-05-16 13:29:43 +0200

    Last resolved : (Sort arrays  1 7kyu)

---
+++ Ruby/7_Kyu/7kyu_sort_arrays_1.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# SORT ARRAYS  1 (7 kyu)
+
+# Instructions
+# Just a simple sorting usage. Create a function that returns the elements of the input-array / list in a sorted manner.
+
+# Solution
+def sortme(n)
+  n.sort
+end
+
+
+# codewars_id: 51f41b98e8f176e70d0002a8   |   16/05/2021 - 11:01:07
