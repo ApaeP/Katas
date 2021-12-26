7312895c2413ab4c1608135a35a329030beff517
Paul Portier <paulportier@gmail.com>
2021-12-26 17:36:34 +0100

    Last resolved : (Float Precision 7kyu)

---
+++ Ruby/7_Kyu/7kyu_float_precision.rb
@@ -0,0 +1,15 @@
+# Codewars - Kata Solution
+# FLOAT PRECISION (7 kyu)
+
+# Instructions
+# Update the solution method to round the argument value to the closest precision of two. The argument will always be a float.
+# solution(23.23456) # should == 23.23
+# solution(1.546) # should == 1.55
+
+# Solution
+def solution(v)
+  v.round(2)
+end
+
+
+# codewars_id: 5143d157ceb46d6a61000001   |   30/07/2021 - 21:16:21
