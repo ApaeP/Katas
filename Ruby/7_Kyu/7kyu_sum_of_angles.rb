07bf16afd3a9828ea98ea22d8be4585a85404017
Paul Portier <paulportier@gmail.com>
2021-12-26 17:35:14 +0100

    Last resolved : (Sum of angles 7kyu)

---
+++ Ruby/7_Kyu/7kyu_sum_of_angles.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# SUM OF ANGLES (7 kyu)
+
+# Instructions
+# Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+# Solution
+def angle(n)
+   ( n - 2 ) * 180
+end
+
+
+# codewars_id: 5a03b3f6a1c9040084001765   |   01/12/2021 - 04:27:45
