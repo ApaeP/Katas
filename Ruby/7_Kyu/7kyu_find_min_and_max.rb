73c82061126bc6536339c74ce13cb2a6fa8bb1a3
Paul Portier <paulportier@gmail.com>
2021-12-26 17:36:44 +0100

    Last resolved : (Find min and max 7kyu)

---
+++ Ruby/7_Kyu/7kyu_find_min_and_max.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# FIND MIN AND MAX (7 kyu)
+
+# Instructions
+# Implement a function that returns the minimal and the maximal value of a list (in this order).
+
+# Solution
+def get_min_max(seq)
+  seq.minmax
+end
+
+
+# codewars_id: 57a1ae8c7cb1f31e4e000130   |   28/07/2021 - 22:31:58
