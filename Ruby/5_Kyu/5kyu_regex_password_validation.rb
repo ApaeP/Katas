efa8e01d9fe6d9cfca1b9b66a22e9f0d11667701
Paul Portier <paulportier@gmail.com>
2021-12-26 17:38:08 +0100

    Last resolved : (Regex Password Validation 5kyu)

---
+++ Ruby/5_Kyu/5kyu_regex_password_validation.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# REGEX PASSWORD VALIDATION (5 kyu)
+
+# Instructions
+# You need to write regex that will validate a password to make sure it meets the following criteria:
+# At least six characters long
+# contains a lowercase letter
+# contains an uppercase letter
+# contains a number
+# Valid passwords will only be alphanumeric characters.
+
+# Solution
+regex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/
+
+
+# codewars_id: 52e1476c8147a7547a000811   |   23/05/2021 - 18:51:22
