3cd27cc52b6a374e6dd13551cf14226b94613bce
Paul Portier <paulportier@gmail.com>
2021-12-26 17:37:39 +0100

    Last resolved : (Unique numbers 7kyu)

---
+++ Ruby/7_Kyu/7kyu_unique_numbers.rb
@@ -0,0 +1,16 @@
+# Codewars - Kata Solution
+# UNIQUE NUMBERS (7 kyu)
+
+# Instructions
+# You have to write a function which returns unique numbers array. You don't need to validate input arg arr. Retain the original order of the input.
+# Examples:
+# unique_numbers([1,1,2,2]) => [1,2]
+# unique_numbers([1,2,3,1]) => [1,2,3]
+
+# Solution
+def unique_numbers(numbers_array)
+  numbers_array.uniq
+end
+
+
+# codewars_id: 568f9bd9cbe89334c900000c   |   25/07/2021 - 00:05:30
