3e77dc0add2a0cc3851d8b9151979624278deb11
Paul Portier <paulportier@gmail.com>
2021-12-26 17:36:58 +0100

    Last resolved : (Greet Me 7kyu)

---
+++ Ruby/7_Kyu/7kyu_greet_me.rb
@@ -0,0 +1,14 @@
+# Codewars - Kata Solution
+# GREET ME (7 kyu)
+
+# Instructions
+# Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+# Example:
+# "riley" --> "Hello Riley!"
+# "JACK"  --> "Hello Jack!"
+
+# Solution
+def greet(name) "Hello #{name.capitalize}!" end
+
+
+# codewars_id: 535474308bb336c9980006f2   |   25/07/2021 - 22:18:07
