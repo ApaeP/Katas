5f37451e0a8a42f69c49ce010f4781ebbf29b9a5
Paul Portier <paulportier@gmail.com>
2021-12-26 17:35:13 +0100

    Last resolved : (Largest Square Inside A Circle 7kyu)

---
+++ Ruby/7_Kyu/7kyu_largest_square_inside_a_circle.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# LARGEST SQUARE INSIDE A CIRCLE (7 kyu)
+
+# Instructions
+# Determine the area of the largest square that can fit inside a circle with radius r.
+
+# Solution
+def area_largest_square(r)
+  ((r*2) ** 2) / 2
+end
+
+
+# codewars_id: 5887a6fe0cfe64850800161c   |   21/12/2021 - 12:13:50
