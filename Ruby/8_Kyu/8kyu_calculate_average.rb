a675742da02752bdf7ff44994738dbfc2d8af728
Paul Portier <paulportier@gmail.com>
2021-12-26 17:38:03 +0100

    Last resolved : (Calculate average 8kyu)

---
+++ Ruby/8_Kyu/8kyu_calculate_average.rb
@@ -0,0 +1,13 @@
+# Codewars - Kata Solution
+# CALCULATE AVERAGE (8 kyu)
+
+# Instructions
+# Write a function which calculates the average of the numbers in a given list.
+# Note: Empty arrays should return 0.
+
+# Solution
+cript
+const find_average = (array) => { return array.reduce((a, b) => a + b, 0) / array.length }
+
+
+# codewars_id: 57a2013acf1fa5bfc4000921   |   01/06/2021 - 23:47:25
