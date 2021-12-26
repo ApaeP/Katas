1c41f1c87717d52361784ffb4ddfdc1378ae3833
Paul Portier <paulportier@gmail.com>
2021-12-26 17:35:33 +0100

    Last resolved : (Xmas Tree 7kyu)

---
+++ Ruby/7_Kyu/7kyu_xmas_tree.rb
@@ -0,0 +1,44 @@
+# Codewars - Kata Solution
+# XMAS TREE (7 kyu)
+
+# Instructions
+# Complete the function that returns a christmas tree of the given height. The height is passed through to the function and the function should return a list containing each line of the tree.
+# XMasTree(5) should return : ['____#____', '___###___', '__#####__', '_#######_', '#########', '____#____', '____#____']
+# XMasTree(3) should return : ['__#__', '_###_', '#####', '__#__', '__#__']
+# Pad with underscores (_) so each line is the same length. Also remember the trunk/stem of the tree.
+# Examples
+# The final idea is for the tree to look like this if you decide to print each element of the list:
+# n = 5 will result in:
+# ____#____              1
+# ___###___              2
+# __#####__              3
+# _#######_              4
+# #########       -----> 5 - Height of Tree
+# ____#____        1      
+# ____#____        2 - Trunk/Stem of Tree
+# n = 3 will result in:
+# __#__                  1
+# _###_                  2
+# #####          ----->  3 - Height of Tree
+# __#__           1
+# __#__           2 - Trunk/Stem of Tree
+
+# Solution
+module CustomAppend
+  def append_n(n, thing)
+    n.times { self << thing }
+    self
+  end
+end
+
+class Array
+  include CustomAppend
+end
+
+def xmas_tree(n)
+  n -= 1
+  [*0..n].map { |e| "#{s = '_' * (n - e) + '#' * e}##{s.reverse}" }.append_n(2, "#{s = '_' * n}##{s}",)
+end
+
+
+# codewars_id: 577c349edf78c178a1000108   |   16/09/2021 - 23:25:13
