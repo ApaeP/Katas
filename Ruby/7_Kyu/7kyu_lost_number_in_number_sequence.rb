0f52677665bd34338f1076f8e82efe2ef55e7459
Paul Portier <paulportier@gmail.com>
2021-12-26 17:38:10 +0100

    Last resolved : (Lost number in number sequence 7kyu)

---
+++ Ruby/7_Kyu/7kyu_lost_number_in_number_sequence.rb
@@ -0,0 +1,19 @@
+# Codewars - Kata Solution
+# LOST NUMBER IN NUMBER SEQUENCE (7 kyu)
+
+# Instructions
+# An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+# Example:
+# The starting array sequence is [1,2,3,4,5,6,7,8,9]
+# The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+# Your function should return the int 5.
+# If no number was deleted from the starting array, your function should return the int 0.
+# Note: N may be 1 or less (in the latter case, the first array will be []).
+
+# Solution
+def find_deleted_number(arr, mixed_arr)
+  (arr - mixed_arr).first || 0
+end
+
+
+# codewars_id: 595aa94353e43a8746000120   |   20/05/2021 - 18:13:16
